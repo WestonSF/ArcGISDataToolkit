2bfaaddcfc6a0370b1c1c3984405aa5a9cf7d919
Shaun Weston <shaun_weston@eagle.co.nz>
2015-05-25 17:17:09 +1200

    Update to NZAAArchSiteDataDownload.bat.

--- Examples/NZAAArchSiteDataDownload.bat
+++ Examples/NZAAArchSiteDataDownload.bat
@@ -1,7 +1,7 @@
 REM --- NZAA ArchSite Data Download ---
 c:\python27\arcgis10.2\python "C:\Data\Tools & Scripts\ArcGIS Data Toolkit\NZAAArchSiteDataDownload.py" ^
  "Transpower" ^
- "Tr@nsp0wer" ^
+ "*****" ^
  "File Geodatabase" ^
  "C:\Temp\Scratch.gdb" ^
  "New"
