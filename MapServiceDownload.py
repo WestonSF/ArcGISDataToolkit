3eb56869a73237676c967805236eff9634ae9cd5
Shaun Weston <shaun_weston@eagle.co.nz>
2015-11-06 09:51:31 +1300

    Update to map service download

--- MapServiceDownload.py
+++ MapServiceDownload.py
@@ -19,6 +19,7 @@ import logging
 import smtplib
 import arcpy
 import json
+import urllib
 import urllib2
 import uuid
 
@@ -44,38 +45,76 @@ def mainFunction(mapServiceLayer,outputFeatureClass,updateMode): # Get parameter
     try:
         # --------------------------------------- Start of code --------------------------------------- #
 
-        # Querying thet map service to get the json
+        # Querying thet map service to get the count of records
         arcpy.AddMessage("Querying the map service...")
-        mapServiceQuery = mapServiceLayer + "/query?where=1%3D1&outFields=*&f=pjson"
-        arcpy.AddMessage("Map Service request made - " + mapServiceQuery)        
-        response = urllib2.urlopen(mapServiceQuery)
+        mapServiceQuery1 = mapServiceLayer + "/query?where=1%3D1&returnIdsOnly=true&f=pjson"
+        urlResponse = urllib.urlopen(mapServiceQuery1);
+        # Get json for the response - Object IDs
+        mapServiceQuery1JSONData = json.loads(urlResponse.read())
+        objectIDs = mapServiceQuery1JSONData["objectIds"]
+        objectIDs.sort()
+        arcpy.AddMessage("Number of records in the layer - " + str(len(objectIDs)) + "...")
+        # Set the number of records per request and the number of requests that need to be made
+        maxRecords = 1000
+        requestsToMake = 1 + (len(objectIDs) / maxRecords)
 
-        # Download the data
-        arcpy.AddMessage("Downloading data...")
-        fileChunk = 16 * 1024
-        downloadedFile = os.path.join(arcpy.env.scratchFolder, "Data-" + str(uuid.uuid1()) + ".json")
-        with open(downloadedFile, 'wb') as file:
-            downloadCount = 0
-            while True:
-                chunk = response.read(fileChunk)
-                # If data size is small
-                if ((downloadCount == 0) and (len(chunk) < 1000)):
-                    # Log error and end download
-                    arcpy.AddError("No data returned, check the URL...")  
-                    sys.exit()
-                if not chunk:
-                    break
-                # Write chunk to output file
-                file.write(chunk)
-                downloadCount = downloadCount + 1
-        file.close()
-        arcpy.AddMessage("Downloaded to " + downloadedFile + "...")
-    
+        # For every request
+        count = 0
+        arcpy.AddMessage("Downloading data to " + arcpy.env.scratchFolder + "...")
+        while (int(requestsToMake) > count):
+            # Create the query
+            startObjectID = int(objectIDs[count*maxRecords])
+            # If at the final request
+            if (int(requestsToMake) == (count+1)):
+                # Get the last object ID
+                endObjectID = int(objectIDs[len(objectIDs)])
+            else:
+                endObjectID = int(objectIDs[startObjectID+maxRecords])
+            serviceQuery = "OBJECTID >= " + str(startObjectID) + " AND OBJECTID < " + str(endObjectID)
+                
+            # Query the map service to data in json format   
+            mapServiceQuery2 = mapServiceLayer + "/query?where=" + serviceQuery + "&returnCountOnly=false&returnIdsOnly=false&returnGeometry=true&outFields=*&f=pjson"
+            response = urllib2.urlopen(mapServiceQuery2)  
+            
+            # Download the data
+            fileChunk = 16 * 1024
+            downloadedFile = os.path.join(arcpy.env.scratchFolder, "Data-" + str(uuid.uuid1()) + ".json")
+            with open(downloadedFile, 'wb') as file:
+                downloadCount = 0
+                while True:
+                    chunk = response.read(fileChunk)
+                    # If data size is small
+                    if ((downloadCount == 0) and (len(chunk) < 1000)):
+                        # Log error and end download
+                        arcpy.AddError("No data returned, check the URL...")  
+                        sys.exit()
+                    if not chunk:
+                        break
+                    # Write chunk to output file
+                    file.write(chunk)
+                    downloadCount = downloadCount + 1
+            file.close()
+     
+            # If it's the first request
+            if (count == 0):
+                # Create new dataset
+                arcpy.JSONToFeatures_conversion(downloadedFile, os.path.join(arcpy.env.scratchGDB, "Dataset"))
+            else:
+                # Create dataset and load into existing
+                arcpy.JSONToFeatures_conversion(downloadedFile, "in_memory\\DatasetTemp")
+                arcpy.Append_management("in_memory\\DatasetTemp", os.path.join(arcpy.env.scratchGDB, "Dataset"), "NO_TEST", "", "")           
+
+            # If at the final request
+            if (int(requestsToMake) == (count+1)):
+                arcpy.AddMessage("Downloaded and converted JSON for " + str(len(objectIDs)) + " of " + str(len(objectIDs)) + " features...")                
+            else:
+                arcpy.AddMessage("Downloaded and converted JSON for " + str(count*maxRecords) + " of " + str(len(objectIDs)) + " features...")
+            count = count + 1
+            
         # Convert JSON to feature class
-        arcpy.AddMessage("Converting JSON to feature class...")
+        arcpy.AddMessage("Copying over final dataset...")
         # Overwrite dataset
         if (updateMode.lower() == "new"):
-            arcpy.JSONToFeatures_conversion(downloadedFile, os.path.join(arcpy.env.scratchGDB, "Dataset"))
             # Get record count
             recordCount = arcpy.GetCount_management(os.path.join(arcpy.env.scratchGDB, "Dataset"))
             arcpy.AddMessage("Number of records for " + outputFeatureClass + " - " + str(recordCount))  
@@ -88,7 +127,6 @@ def mainFunction(mapServiceLayer,outputFeatureClass,updateMode): # Get parameter
                 arcpy.CopyFeatures_management(os.path.join(arcpy.env.scratchGDB, "Dataset"), outputFeatureClass, "", "0", "0", "0")
         # Delete and append
         else:
-            arcpy.JSONToFeatures_conversion(downloadedFile, os.path.join(arcpy.env.scratchGDB, "Dataset"))
             # Get record count
             recordCount = arcpy.GetCount_management(os.path.join(arcpy.env.scratchGDB, "Dataset"))
             arcpy.AddMessage("Number of records for " + outputFeatureClass + " - " + str(recordCount))  
