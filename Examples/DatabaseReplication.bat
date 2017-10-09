84e780837b1d920b49ae621acef76a18b190f1de
Shaun Weston <shaun_weston@eagle.co.nz>
2017-10-09 20:34:04 +1300

    Adding of exclusion list to database replication tool.

--- Examples/DatabaseReplication.bat
+++
@@ -1,7 +0,0 @@
-REM ----- Database Replication -----
-c:\python27\arcgis10.2\python "C:\Development\Projects\ArcGIS Data Toolkit\DatabaseReplication.py" ^
- "C:\Projects\Wellington City Council - GIS Upgrade\Data\WCCOld (gisadmin).sde" ^
- "C:\Projects\Wellington City Council - GIS Upgrade\Data\WCCNew (gisadmin).sde" ^
- "Only From Configuration File" ^
- "Existing" ^
- "C:\Development\Projects\ArcGIS Data Toolkit\Configuration\ConfigDatabaseReplication-Wellington.csv"
